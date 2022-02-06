0045fe928181729b649b01b8f95a2fbb7928fa0b
Никита Лыкосов <nikita.likoosv@yandex.ru>
2022-02-07 03:04:24 +0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return <span>Helloy word</span>
+}
+
+export default Home
