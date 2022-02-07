c99da6c6159b7bf7ceb1759532e92528a4e1dbfa
Никита Лыкосов <nikita.likoosv@yandex.ru>
2022-02-07 13:02:16 +0500

    add view auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,83 @@
 import type { NextPage } from 'next'
 import { Layout } from 'antd'
-import bgLanding from '../assets/img/bgIm.jpg'
+import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import Link from 'next/link'
 
 const { Header, Footer, Content } = Layout
 
-const Home: NextPage = () => {
+const Auth: NextPage = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
   return (
-    <>
-      <Layout>
-        <Layout
-          style={{
-            background: `fixed none url(${bgLanding.src}) center cover`,
-            minHeight: '90vh',
-          }}
+    <div
+      style={{
+        minHeight: '100vh',
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Пожалуйста введите логин!',
+            },
+          ]}
         >
-          <Header style={{ background: 'none' }}>schools ai</Header>
-          <Content style={{ background: 'none' }}>Content</Content>
-        </Layout>
-        <Footer style={{ background: 'none' }}>Footer</Footer>
-      </Layout>
-    </>
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Логин"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Пожалуйста введите пароль!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Пароль"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button style={{ display: 'block' }} type="primary" htmlType="submit">
+            Войти
+          </Button>
+          Или
+          <Link href="/registration">
+            <a>Зарегистриуйтесь</a>
+          </Link>
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
 
-export default Home
+export default Auth
