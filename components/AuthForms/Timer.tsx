be826b2d88ae37f355b05ea41cae7a658893b4d1
JIiCeHoK <foxs29546@gmail.com>
2022-07-11 15:37:30 +0500

    Fix: исправлен таймер

--- components/AuthForms/Timer.tsx
+++ components/AuthForms/Timer.tsx
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Timer: React.FC<{ onFinish: () => void }> = ({ onFinish }) => {
-  const [time, setTime] = useState(60)
-  const calcTime = (time: number) => {
-    let timeStr = time.toString()
-    if (time <= 0) {
-      return `00:00`
-    }
-    switch (timeStr.length) {
-      case 1:
-        return time <= 0 ? '00:00' : `00:0${timeStr}`
-      case 2:
-        return time < 60 ? `00:${timeStr}` : `01:00`
-      default:
-        return `00:00`
-    }
-  }
-  useEffect(() => {
-    const tick = () => {
-      setTime((el) => --el)
-      if (time >= 0) {
-        setTimeout(tick, 1000)
-      } else onFinish()
-    }
-    setTimeout(tick, 1000)
-  }, [time])
+    const [time, setTime] = useState(60);
+    const calcTime = (time: number) => {
+        let timeStr = time.toString();
+        if (time <= 0) {
+            return `00:00`;
+        }
+        switch (timeStr.length) {
+            case 1:
+                return time <= 0 ? "00:00" : `00:0${timeStr}`;
+            case 2:
+                return time < 60 ? `00:${timeStr}` : `01:00`;
+            default:
+                return `00:00`;
+        }
+    };
+    useEffect(() => {
+        const tick = () => {
+            setTime((el) => el - 1);
+        };
+        if (time >= 0) {
+            setTimeout(tick, 1000);
+        } else onFinish();
+    });
 
-  return <>{calcTime(time)}</>
-}
+    return <>{calcTime(time)}</>;
+};
 
-export default Timer
+export default Timer;
